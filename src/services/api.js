f7043f1dd604ea839d3632ae6538f265d8e4a5d8
Marto <martin@pirkastone.com>
2025-04-10 21:53:12 -0300

    ajuste api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const getPosts = async () => {
-  const res = await axios.get('/src/data/posts.json') // Cambiá esto por tu URL real luego
-  return res.data
-}
+  const res = await axios.get("/data/posts.json"); // Cambiá esto por tu URL real luego
+  return res.data;
+};
